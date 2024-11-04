a1b72e6b152b4ea1df5424757c00eb89f57c6dc4
Std-user <adrianmarquez1607@gmail.com>
2024-11-04 09:19:11 -0400

    Add files via upload

---
+++ MATRIX.cpp
@@ -0,0 +1,14 @@
+//Bienvenido a la matrix
+#include <iostream>
+using namespace std;
+
+int main(){
+	string nombre;
+	
+	cout<<"Ingrese su nombre: ";
+	cin>>nombre;
+	
+	cout<<"Ahora estas en la matrix, "<<nombre;
+	
+	return 0;
+}
